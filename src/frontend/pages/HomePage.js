7f2b70c120a38f1df87dd73f6b563de9ce1aa5b7
suyeon <dks2sy@naver.com>
2025-01-26 23:02:07 +0900

    홈 화면 구성

--- src/frontend/pages/HomePage.js
+++ src/frontend/pages/HomePage.js
@@ -1,62 +1,67 @@
-import React, { useState, useRef, useMemo } from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import  BottomSheet from "@gorhom/bottom-sheet";
+import React, { useState, useRef, useMemo } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import BottomSheet, { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 //import { Calendar } from "react-native-calendars";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function App() {
+export default function HomeScreen() {
   // today
   const currentDate = new Date();
   let month = currentDate.getMonth() + 1;
   let date = currentDate.getDate();
-  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
   const dayOfWeek = daysOfWeek[currentDate.getDay()];
   //bottomSheet
   const bottomSheetRef = useRef(null);
-  const snapPoints = useMemo(() => ['25%', '50%', '100%'], []);
+  const snapPoints = useMemo(() => ["25%", "50%", "100%"], []);
   const openBottomSheet = () => {
     if (bottomSheetRef.current) {
-      bottomSheetRef.current.expand(); // 
+      bottomSheetRef.current?.present(); //
     }
   };
 
-  
-
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}> 
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
         <SafeAreaView style={styles.container}>
           <StatusBar style="auto" />
           <View style={styles.parent}>
-            <View style={styles.header}></View> {/*header */}
+            <View style={styles.header}></View>
             <View style={styles.home}>
-              <View style={styles.calendarArea}> {/* calendar */}
+              <View style={styles.calendarArea}>
                 <View style={{ flexDirection: "row" }}>
                   <Text style={styles.calendar}>캘린더</Text>
                   <TouchableOpacity
                     style={styles.openButton}
                     onPress={openBottomSheet}
                   >
-                    <Text style={styles.buttonText}>{'>'}</Text>
+                    <Text style={styles.buttonText}>{">"}</Text>
                   </TouchableOpacity>
 
-                  {/* Bottom Sheet  */}
-                  <BottomSheet
-                      ref={bottomSheetRef}
-                      snapPoints={snapPoints}
-                      index={0}  
-                      enablePanDownToClose={true}
-                      
-                    
-                  >       
+                  <BottomSheetModal
+                    ref={bottomSheetRef}
+                    snapPoints={snapPoints}
+                    index={0}
+                    enablePanDownToClose={true}
+                  >
                     <View style={styles.bottomSheetContent}>
                       <Text style={styles.bottomSheetText}>캘린더 구현</Text>
                     </View>
-                    </BottomSheet>
+                  </BottomSheetModal>
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                  <Text style={styles.todaymonthdate}>{month}.{date}</Text>
+                  <Text style={styles.todaymonthdate}>
+                    {month}.{date}
+                  </Text>
                   <Text style={styles.todaydayofweek}>{dayOfWeek}</Text>
                   <Text style={styles.bar}>|</Text>
                   <Text style={styles.todaySchdule}>pm 18:00 윤태원 학생</Text>
@@ -64,16 +69,17 @@ export default function App() {
                 <Text style={styles.calendar7days}>일주일 단위 구현</Text>
               </View>
               <View style={styles.classjournalArea}>
-                <Text style={{ fontWeight: "bold", fontSize: 18 }}>수업일지</Text>
-              </View> {/* 수업일지*/}
+                <Text style={{ fontWeight: "bold", fontSize: 18 }}>
+                  수업일지
+                </Text>
+              </View>
             </View>
           </View>
         </SafeAreaView>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   //상단, 하단바
@@ -101,7 +107,6 @@ const styles = StyleSheet.create({
     //flex: 1,
     paddingLeft: 20,
     paddingBottom: 350,
-
   },
   calendar: {
     fontWeight: "bold",
@@ -138,7 +143,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#78a9ed",
     padding: 15,
     borderRadius: 50,
-    justifyContent: 'center',
+    justifyContent: "center",
     alignItems: "center",
     marginTop: 25,
     marginLeft: 1,
@@ -158,7 +163,5 @@ const styles = StyleSheet.create({
   },
   bottomSheetText: {
     fontSize: 18,
-    
   },
-
 });
